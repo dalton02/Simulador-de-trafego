9e4248b34c649a529e062d7da898f4cf54884d40
daltonPinaculo <dalton@pinaculodigital.com.br>
2026-02-23 09:06:23 -0300

    .

---
+++ src/core/Grid.cpp
@@ -0,0 +1,8 @@
+
+
+#include "../../include/core/Grid.h"
+#include <iostream>
+
+using namespace std;
+
+void x() { cout << "AAAAAA"; }
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "../include/core/Grid.h"
+#include <cstdio>
+
+int main() {
+
+  x();
+
+  return 0;
+}
