7862af0f534f85381f89eeeb01341d4fb647ceb9
Vinicius Inacio dos Santos <viniciusisantos07@gmail.com>
2026-03-12 23:36:15 -0300

    estrutura da classe do semaforo

---
+++ include/core/traffic_light.hpp
@@ -0,0 +1,32 @@
+#ifndef CLOCK_HPP
+#define CLOCK_HPP
+
+#include "../utils/utils.hpp"
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+#include <semaphore>
+
+class TrafficLight {
+
+private:
+  int ticksForToggle;
+  int ticks;
+  void toggle();
+
+public:
+  bool green;
+  int direction;
+  bool keepClosed;
+  int closest;
+  Object obj;
+  std::condition_variable cv;
+  std::mutex mu;
+
+  TrafficLight(Object obj, int direction, int ticksForToggle, int startWith);
+  ~TrafficLight();
+};
+
+extern std::deque<TrafficLight> globalLights;
+
+#endif
